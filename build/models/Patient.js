c35c96824e7225e86aa8fb205f298fae12926013
Alan Manuel Loreto Cornídez <aloretocornidez@arizona.edu>
2022-11-28 08:59:52 -0700

    before school

--- build/models/Patient.js
+++ build/models/Patient.js
@@ -3,26 +3,30 @@ const Schema = mongoose.Schema;
 
 
 const patientSchema = new Schema({
-
-    firstName: {
+    // First Name
+    name: {
         type: String,
         required: true
     },
-    lastName: {
-        type: String,
-        required: true
-    },
+    // Username storage.
     username: {
         type: String,
         required: true
     },
+    // Password hash storage.
     passwordHash: {
         type: String,
         required: true
     },
+    // Including an array of readings for the patient.
     readings: {
-        type: []
+        type:
+            [{
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'Reading'
+            }]
     },
+    // The patient has a physician.
     physician: {
 
         type: mongoose.Schema.Types.ObjectId,
--- build/models/Physician.js
+++ build/models/Physician.js
@@ -4,27 +4,28 @@ const Schema = mongoose.Schema;
 
 const physicianSchema = new Schema({
 
-    firstName: {
+    name: {
         type: String,
         required: true
     },
-    lastName: {
-        type: String,
-        required: true
-    },
+    // Username storage
     username: {
         type: String,
-        required: true
+        required: true,
+        min: 1,
+        max: 1
     },
+    // Password storage.
     passwordHash: {
         type: String,
         required: true
     },
+    // The Physician stores an array of patients
     patients: {
-        type: {
+        type: [{
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Patient'
-        }
+        }]
     }
 
 });
