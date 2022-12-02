b91ef34fc0e3fdb32fecd493c251bcf57c476245
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2022-12-02 15:42:49 -0700

    made a change that is amazing

---
+++ build/routes/devices.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+//Eventually store these in the database
+var start_hour = 6; //Use military time hours
+var end_hour = 22;
+var interval = 30; //In minutes
+
+router("/timeReq", function(req, res){
+  res.status(200).json({start: start_hour, end: end_hour, time_int: interval});
+})
+
+
+
+
+module.exports = router;
