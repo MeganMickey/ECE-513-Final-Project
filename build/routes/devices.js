eb690f0cca8752e98b9f7314414a8633a4a027df
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2022-12-04 11:40:36 -0700

    Update devices.js

--- build/routes/devices.js
+++ build/routes/devices.js
@@ -6,7 +6,7 @@ var start_hour = 6; //Use military time hours
 var end_hour = 22;
 var interval = 30; //In minutes
 
-router("/timeReq", function(req, res){
+router.post("/timeReq", function(req, res){
   res.status(200).json({start: start_hour, end: end_hour, time_int: interval});
 })
 
