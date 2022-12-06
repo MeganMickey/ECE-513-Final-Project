35186314ead8f20411e44e0246bd2b7b4c04f1ed
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2022-12-06 14:30:22 -0700

    Update devices.js

--- build/routes/devices.js
+++ build/routes/devices.js
@@ -15,7 +15,7 @@ router.post("/timeReq", function(req, res){
 });
 
 router.post("/healthData", function(req, res){
-  console.log("made it to request");
+  console.log(req.body);
   const newReading = new Reading({
     time: req.body.rdtm,
     heartRate: req.body.hr,
