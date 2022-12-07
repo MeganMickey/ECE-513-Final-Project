bb426e960086b83c58c68cdbc792f29b0a8871c6
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2022-12-07 16:09:13 -0700

    update signup.js

--- build/public/javascripts/signup.js
+++ build/public/javascripts/signup.js
@@ -1,26 +1,6 @@
 // Javascript that will run whenever the signup page is loaded.
 
-console.log('window.onload: javascript/signup.js');
-
-
-
-
-
-var submitElement = document.getElementsByTagName('input');
-
-
-
-let formItems = $("div.form-item input");
-
-
-var arr = [].slice.call(submitElement);
-console.log(arr);
-*/
-
-let form = document.getElementsByClassName('register-form-content').getElementById('name');
-
-
-console.log(form);
+var submitElement = document.getElementById('sign-up-button');
 
 submitElement.addEventListener("click", function () {
 
@@ -49,13 +29,14 @@ submitElement.addEventListener("click", function () {
 
     if (name.length < 1) {
         errorString += "<li>Missing full name.</li>";
-        document.getElementById("fullName").style.borderWidth = "2px";
-        document.getElementById("fullName").style.borderColor = "red";
+        document.getElementById("name").style.borderWidth = "2px";
+        document.getElementById("name").style.borderColor = "red";
 
     }
 
     email_re = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,5}$/;
     if (!email_re.test(email)) {
+        //window.alert("Invalid or missing email address.");
         errorString += "<li>Invalid or missing email address.</li>"
         document.getElementById("email").style.borderWidth = "2px";
         document.getElementById("email").style.borderColor = "red";
@@ -63,6 +44,7 @@ submitElement.addEventListener("click", function () {
 
 
     if (password.length < 10 || password.length > 20) {
+
         errorString += "<li>Password must be between 10 and 20 characters.</li>"
         document.getElementById("password").style.borderWidth = "2px";
         document.getElementById("password").style.borderColor = "red";
@@ -96,10 +78,49 @@ submitElement.addEventListener("click", function () {
         document.getElementById("passwordConfirm").style.borderWidth = "2px";
     }
 
-    error.innerHTML = "<ul>" + errorString + "</ul>";
+    error.innerHTML = errorString;
     if (errorString.length > 1) {
         error.style.display = "block";
     }
+    
+    if(errorString == ""){
+        let txdata = {
+            name: $('#name').val(),
+            username: $('#email').val(),
+            password: $('#password').val()
+        }
+        const btns = document.querySelectorAll('input[name="twoChoice"]');
+        let selected;
+        for(const btn of btns){
+            if(btn.checked){
+                selected = btn.value;
+                break;
+            }
+        }
+        if(selected == "patient"){
+            $.ajax({
+                url: '/patients/signUp',
+                method: 'POST',
+                contentType: 'application/json',
+                data: JSON.stringify(txdata),
+                dataType: 'json'
+            })
+            .done(function (data, textStatus, jqXHR) {
+                $('#formErrors').html(JSON.stringify(data, null, 2));
+                if (data.success) {
+                    // after 1 second, move to "login.html"
+                    setTimeout(function(){
+                        window.location = "login.html";
+                    }, 1000);
+                }
+            })
+            .fail(function (jqXHR, textStatus, errorThrown) {
+                if (jqXHR.status == 404) {
+                    $('#formErrors').html("Server could not be reached!!!");    
+                }
+                else $('#formErrors').html(JSON.stringify(jqXHR, null, 2));
+            });
+        }
+    }
 
 });
-*/
