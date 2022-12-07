59af461242f46a5e070c1094fe19a645e90957e3
Alan Manuel <aloretocornidez@arizona.edu>
2022-12-07 16:16:09 -0700

    test

--- build/db.js
+++ build/db.js
@@ -1,6 +1,6 @@
 // to use mongoDB
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1/ece-513", { useNewUrlParser: true, useUnifiedTopology:true });
+mongoose.connect("mongodb://localhost:3000/webdevelopment", { useNewUrlParser: true, useUnifiedTopology:true });
 
 
 module.exports = mongoose;
