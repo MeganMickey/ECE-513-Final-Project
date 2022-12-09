367d3580dc13b93ccda496b6fe7b0107c7364da7
MeganMickey <67599197+MeganMickey@users.noreply.github.com>
2022-12-09 11:16:49 -0700

    adding embedded programming

---
+++ build/simple_hr/target/4.0.1/argon/obj/src/user_module.o.d
@@ -0,0 +1,6 @@
+c\:/Users/megan/College/Senior/Web_Development/Heartbeat_Project/simple_hr/target/4.0.1/argon//obj/./src/user_module.o: \
+ src/user_module.c \
+ ../../../modules/shared/nRF52840/inc/user-part/user_module.inc \
+ ../../../dynalib/inc/dynalib.h
+../../../modules/shared/nRF52840/inc/user-part/user_module.inc:
+../../../dynalib/inc/dynalib.h:
